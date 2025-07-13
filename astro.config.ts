f20d59d3ce43ee8f2b028fcaaf0695509b0cb1b7
amrbr <amrbreekaa@gmail.com>
2025-07-13 13:26:20 +0300

    some other updates

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,19 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  base: '/',
+  output: 'static',
+  trailingSlash: 'never',
+  build: {
+    assets: '_astro'
+  },
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: 'assets/[name].[hash][extname]'
+        }
+      }
+    }
+  }
 });
